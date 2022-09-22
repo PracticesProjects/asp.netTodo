5fd0a8a9fa0f1d7ecbc7e32f94b0b1858be2bb88
Sagar1555 <stimalsena155@gmail.com>
2022-09-22 10:05:18 +0545

    migration file update

--- Migrations/ApplicationDbContextModelSnapshot.cs
+++ Migrations/ApplicationDbContextModelSnapshot.cs
@@ -3,19 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SagarNote.Data;
 
 #nullable disable
 
-namespace SagarNote.Data.Migrations
+namespace SagarNote.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20220922041308_Model Created")]
-    partial class ModelCreated
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
